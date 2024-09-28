2b1185ac0895aad8fe19bb08824e3b9e78930a52
IlinNR <ilinnr@suek.ru>
2024-09-28 09:21:01 +0300

    temporary output in VideoEmbeddingNode

--- nodes/VideoEmbeddingNode.py
+++ nodes/VideoEmbeddingNode.py
@@ -1,9 +1,11 @@
 from elements.VideoElement import VideoElement
 
-class VideoEmbeddingNode():
+
+class VideoEmbeddingNode:
 
     def __init__(self, config: dict) -> None:
         pass
 
     def process(self, video_element: VideoElement):
+        video_element.embedding = [1, 1, 1, 1, 1]
         return video_element
