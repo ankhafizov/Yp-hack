95869a0a6329dd9bed0ae7b8e0366fa258bacdcd
IlinNR <ilinnr@suek.ru>
2024-09-28 09:17:35 +0300

    delete VideClassifierNode

--- nodes/VideoClassifierNode.py
+++
@@ -1,9 +0,0 @@
-from elements.VideoElement import VideoElement
-
-class VideoClassifierNode:
-
-    def __init__(self, config: dict) -> None:
-        pass
-
-    def process(self, video_element: VideoElement):
-        return video_element
--- services/services.py
+++ services/services.py
@@ -1,6 +1,5 @@
 from nodes.VideoEmbeddingNode import VideoEmbeddingNode
 from nodes.VectorDBNode import VectorDBNode
-from nodes.VideoClassifierNode import VideoClassifierNode
 from elements.VideoElement import VideoElement
 import yaml
 
@@ -10,16 +9,13 @@ with open("configs/app_config.yaml", "r") as file:
 
 # Инициализация нод:
 video_embedding_node = VideoEmbeddingNode(config)
-vector_db_node = VectorDBNode(config)
-video_classifier_node = VideoClassifierNode(config)
-
+vector_db_node = VectorDBNode(config["VectorDBNode"])
 
 def check_video_duplicate(video_element: VideoElement):
     ### Функция реализует обработку видео и говорит есть ли дубликат (если есть то кто)
 
     video_element = video_embedding_node.process(video_element)
     video_element = vector_db_node.process_search(video_element)
-    video_element = video_classifier_node.process(video_element)
 
     return video_element.is_dublicate, video_element.top_1_neighbour_uuid
 
