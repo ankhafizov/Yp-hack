714928b0e3dc90ea252d7c16e9520f16c5cf7d05
IlinNR <ilinnr@suek.ru>
2024-09-28 19:44:53 +0300

    example file

---
+++ exampl.py
@@ -0,0 +1,8 @@
+import requests
+# пример запроса на проверку дубликата
+api_url = "http://localhost:8000" 
+check_url = f"{api_url}/check-video-duplicate"
+video_path = "https://s3.ritm.media/yappy-db-duplicates/3c9011da-026a-43d4-84c6-1fe7838a14c4.mp4"
+
+check_response = requests.post(check_url, json={"link": video_path})
+print(check_response.json())
