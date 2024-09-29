17fb38a77a30825598d1723ce0e3a28d00bab81f
Anvar Khafizov <khafizovanv@digtp.com>
2024-09-29 09:33:00 +0300

    last updates

--- example_test.py
+++ example_test.py
@@ -1,6 +1,7 @@
 import requests
+
 # пример запроса на проверку дубликата
-api_url = "http://localhost:8000" 
+api_url = "http://62.68.147.104:8000/docs"
 check_url = f"{api_url}/check-video-duplicate"
 video_path = "https://s3.ritm.media/yappy-db-duplicates/3c9011da-026a-43d4-84c6-1fe7838a14c4.mp4"
 
